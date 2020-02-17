0933582f97345675b348a7ee73ee789000aa382d
Alen Garibović <a.garibovic95@gmail.com>
2020-02-18 00:07:01 +0100

    jQuery validation for input fields added

--- wrd_files/validacija.js
+++ wrd_files/validacija.js
@@ -1,38 +1,54 @@
-// Wait for the DOM to be ready
+
 $(function() {
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
+    jQuery.validator.addMethod("lettersonly", function(value, element) {
+      return this.optional(element) || /^[a-z]+$/i.test(value);
+    }, "Samo slova"); 
+    
+    jQuery.validator.addMethod('phoneUS', function(phone_number, element) {
+      phone_number = phone_number.replace(/\s+/g, ''); 
+      return this.optional(element) || phone_number.length > 9 &&
+          phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+  }, 'Molimo unesite telefon u pravilnom formatu');
+
     $("form[name='registration']").validate({
-      // Specify validation rules
       rules: {
-        // The key name on the left side is the name attribute
-        // of an input field. Validation rules are defined
-        // on the right side
-        firstname: "required",
-        lastname: "required",
-        email: {
+        dostavaIme: {
           required: true,
-          // Specify that email should be validated
-          // by the built-in "email" rule
-          email: true
+          lettersonly: true
         },
-        password: {
+        Address: {
           required: true,
+          lettersonly: true
+        },
+        postalcode: {
+          required: true, 
+          number: true, 
           minlength: 5
-        }
-      },
-      // Specify validation error messages
-      messages: {
-        firstname: "Please enter your firstname",
-        lastname: "Please enter your lastname",
-        password: {
-          required: "Please provide a password",
-          minlength: "Your password must be at least 5 characters long"
         },
-        email: "Please enter a valid email address"
+        Phone: {
+          required: true,
+          phoneUS: true
+        },
       },
-      // Make sure the form is submitted to the destination defined
-      // in the "action" attribute of the form when valid
+      messages: {
+        dostavaIme: {
+          required: "Ime je obavezno",
+          lettersonly: "Samo su slova dozvoljena"
+        },
+        Address: {
+          required: "Adresa je obavezna",
+          lettersonly: "Samo su slova dozvoljena"
+        },
+        postalcode: {
+          required: "Postanski broj je obavezan",
+          number: "Samo su brojevi dozvoljeni", 
+          minlength: "Minimalna duzina 5 brojeva"
+        },
+        Phone: {
+          required: "Telefon je obavezan",
+          phoneUS: "Molimo unesite broj u pravilnom formatu"
+        },
+      },
       submitHandler: function(form) {
         form.submit();
       }
