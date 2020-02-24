580b8e85dfa895ac8ef1627e8ae3c7a2e3b55d3a
Alen Garibović <a.garibovic95@gmail.com>
2020-02-24 23:46:19 +0100

    javascript part done

--- wrd_files/script.js
+++ wrd_files/script.js
@@ -1,27 +1,30 @@
 function obojiZuto() {
 
-    var slike = document.querySelectorAll('img');
+  var slike = document.querySelectorAll('img');
 
-    for(var i = 0; i < slike.length; i++) {
+  for (var i = 0; i < slike.length; i++) {
 
-        slike[i].addEventListener('click', function() {
-            this.setAttribute("style", "border: thick solid yellow;");
-          });
+    slike[i].addEventListener('click', function () {
+      this.setAttribute("style", "border: thick solid yellow;");
+    });
 
-    }
-    
+  }
+
 }
 
 obojiZuto();
+dobaviPodatke();
 
-fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/GetAll')
-        .then((res) => { return res.json() })
-        .then((data) => {
-            let rezultat = '';
-            data.forEach((kupac) => {
-                const { narudzbaId, datumNarudzbe, dostavaIme, dostavaAdresa, dostavaPostanskiBroj, dostavaTelefon, napomena } = kupac
-                 rezultat +=
-                    `<tdead>
+function dobaviPodatke() {
+
+  fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/GetAll')
+    .then((res) => { return res.json() })
+    .then((data) => {
+      let rezultat = '';
+      data.forEach((kupac) => {
+        const { narudzbaId, datumNarudzbe, dostavaIme, dostavaAdresa, dostavaPostanskiBroj, dostavaTelefon, napomena } = kupac
+        rezultat +=
+          `<tdead>
                     <tr>
                     <td>${narudzbaId}</td>
                     <td>${datumNarudzbe}</td>
@@ -34,51 +37,30 @@ fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/GetAll')
                     </tdead>
                     <tbody>
                     </tbody>`;
-                    });
-                    document.getElementById('table').innerHTML += rezultat;
-                })
-            
+      });
+      document.getElementById('table').innerHTML += rezultat;
+    })
 
-document.getElementById('registration').addEventListener('submit', registration);
+}
 
- function registration(event){
-            event.preventDefault();
 
-            let dostavaIme = document.getElementById('dostavaIme').value;
-            let dostavaAdresa = document.getElementById('Address').value;
-            let postalcode = document.getElementById('postalcode').value;
-            let phone = document.getElementById('Phone').value;
-            let napomena = document.getElementById('napomena').value;
-            
-            fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/Dodaj', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                  },
-                body:JSON.stringify({dostavaIme: dostavaIme, dostavaAdresa: dostavaAdresa, dostavaPostanskiBroj: postalcode, dostavaTelefon: phone, napomena: napomena})
-            }).then((res) => res.json())  
-            .then((data) =>  console.log(data))
-            .catch((err)=>console.log(err))
-        }
+function filter() {
 
-        function filter() {
-     
-            var input, filter, table, tr, td, i, txtValue;
-            input = document.getElementById("filtiranje");
-            filter = input.value.toUpperCase();
-            table = document.getElementById("table");
-            tr = table.getElementsByTagName("tr");
-          
-            for (i = 0; i < tr.length; i++) {
-              td = tr[i].getElementsByTagName("td")[2];
-              if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                  tr[i].style.display = "";
-                } else {
-                  tr[i].style.display = "none";
-                }
-              }
-            }
-          }
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("filtiranje");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
--- wrd_files/validacija.js
+++ wrd_files/validacija.js
@@ -1,56 +1,74 @@
 
-$(function() {
-    jQuery.validator.addMethod("lettersonly", function(value, element) {
-      return this.optional(element) || /^[a-z\s]+$/i.test(value);
-    }, "Samo slova"); 
-    
-    jQuery.validator.addMethod('phoneUS', function(phone_number, element) {
-      phone_number = phone_number.replace(/\s+/g, ''); 
-      return this.optional(element) || phone_number.length > 9 &&
-          phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+$(function () {
+  jQuery.validator.addMethod("lettersonly", function (value, element) {
+    return this.optional(element) || /^[a-z\s]+$/i.test(value);
+  }, "Samo slova");
+
+  jQuery.validator.addMethod('phoneUS', function (phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, '');
+    return this.optional(element) || phone_number.length > 9 &&
+      phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
   }, 'Molimo unesite telefon u pravilnom formatu');
 
-    $("form[name='registration']").validate({
-      rules: {
-        dostavaIme: {
-          required: true,
-          lettersonly: true
-        },
-        Address: {
-          required: true,
-          lettersonly: true
-        },
-        postalcode: {
-          required: true, 
-          number: true, 
-          minlength: 5
-        },
-        Phone: {
-          required: true,
-          phoneUS: true
-        },
+  $("form[name='registration']").validate({
+    rules: {
+      dostavaIme: {
+        required: true,
+        lettersonly: true
       },
-      messages: {
-        dostavaIme: {
-          required: "Ime je obavezno",
-          lettersonly: "Samo su slova dozvoljena"
-        },
-        Address: {
-          required: "Adresa je obavezna",
-          lettersonly: "Samo su slova dozvoljena"
-        },
-        postalcode: {
-          required: "Postanski broj je obavezan",
-          number: "Samo su brojevi dozvoljeni", 
-          minlength: "Minimalna duzina 5 brojeva"
-        },
-        Phone: {
-          required: "Telefon je obavezan",
-          phoneUS: "Molimo unesite broj u pravilnom formatu (US number format)"
-        },
+      Address: {
+        required: true,
+        lettersonly: true
       },
-      submitHandler: function(form) {
-        form.submit();
-      }
-    });
+      postalcode: {
+        required: true,
+        number: true,
+        minlength: 5
+      },
+      Phone: {
+        required: true,
+        phoneUS: true
+      },
+    },
+    messages: {
+      dostavaIme: {
+        required: "Ime je obavezno",
+        lettersonly: "Samo su slova dozvoljena"
+      },
+      Address: {
+        required: "Adresa je obavezna",
+        lettersonly: "Samo su slova dozvoljena"
+      },
+      postalcode: {
+        required: "Postanski broj je obavezan",
+        number: "Samo su brojevi dozvoljeni",
+        minlength: "Minimalna duzina 5 brojeva"
+      },
+      Phone: {
+        required: "Telefon je obavezan",
+        phoneUS: "Molimo unesite broj u pravilnom formatu (US number format)"
+      },
+    },
+    submitHandler: function () {
+
+      let dostavaIme = document.getElementById('dostavaIme').value;
+      let dostavaAdresa = document.getElementById('Address').value;
+      let postalcode = document.getElementById('postalcode').value;
+      let phone = document.getElementById('Phone').value;
+      let napomena = document.getElementById('napomena').value;
+
+      fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/Dodaj', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ dostavaIme: dostavaIme, dostavaAdresa: dostavaAdresa, dostavaPostanskiBroj: postalcode, dostavaTelefon: phone, napomena: napomena })
+      }).then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+        .finally(document.getElementById("registration").reset())
+    }
+
   });
+});
