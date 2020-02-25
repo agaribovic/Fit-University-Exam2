c16a5e32afc0f396079e3607d3627161a8978aa3
Alen Garibović <a.garibovic95@gmail.com>
2020-02-25 17:17:23 +0100

    code refactoring

--- wrd_files/script.js
+++ wrd_files/script.js
@@ -43,8 +43,29 @@ function dobaviPodatke() {
 
 }
 
+function posaljiPodatke() {
 
-function filter() {
+  let dostavaIme = document.getElementById('dostavaIme').value;
+  let dostavaAdresa = document.getElementById('Address').value;
+  let postalcode = document.getElementById('postalcode').value;
+  let phone = document.getElementById('Phone').value;
+  let napomena = document.getElementById('napomena').value;
+
+  fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/Dodaj', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ dostavaIme: dostavaIme, dostavaAdresa: dostavaAdresa, dostavaPostanskiBroj: postalcode, dostavaTelefon: phone, napomena: napomena })
+  }).then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+    .finally(document.getElementById("registration").reset())
+
+}
+
+function filtrirajPodatke() {
 
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("filtiranje");
--- wrd_files/validacija.js
+++ wrd_files/validacija.js
@@ -50,24 +50,7 @@ $(function () {
       },
     },
     submitHandler: function () {
-
-      let dostavaIme = document.getElementById('dostavaIme').value;
-      let dostavaAdresa = document.getElementById('Address').value;
-      let postalcode = document.getElementById('postalcode').value;
-      let phone = document.getElementById('Phone').value;
-      let napomena = document.getElementById('napomena').value;
-
-      fetch('http://onlineshop.wrd.app.fit.ba/api/ispit20190622/Narudzba/Dodaj', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ dostavaIme: dostavaIme, dostavaAdresa: dostavaAdresa, dostavaPostanskiBroj: postalcode, dostavaTelefon: phone, napomena: napomena })
-      }).then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
-        .finally(document.getElementById("registration").reset())
+      posaljiPodatke();
     }
 
   });
